7b1e8f08ba2d5270888f71a0268d79649487a9f5
Jonnatan Jossemar Cordero <jonnatan.cordero@suse.com>
2022-03-14 11:22:57 -0600

    fixup! Add script to verify if RKE2 charts are up to date
    Signed-off-by: Jonnatan Jossemar Cordero <jonnatan.cordero@suse.com>

--- bin/librancher-ecm.sh
+++ bin/librancher-ecm.sh
@@ -1,19 +1,23 @@
 #!/bin/sh
 
-. libstd-ecm.sh
-
-__RANCHER_PATH="${GOPATH}/src/github.com/rancher"
 __INVALID_ARG_ERROR="error: update requires an argument"
-__IMAGE_BUILD_REPOS=$(find "${__RANCHER_PATH}" \
-    -path "*image-build-*"                     \
-    -type f -not -path "*image-build-tools/*"  \
-    -type f -not -path "*image-build-base/*"   \
-    -type f -name "Dockerfile")
 
 # rancher_list_local_repos prints out all of the local
 # repos to STDOUT.
 rancher_list_local_repos() {
+    __RANCHER_PATH="${GOPATH}/src/github.com/rancher"
+    __IMAGE_BUILD_REPOS=$(find "${__RANCHER_PATH}" \
+        -path "*image-build-*" \
+        -type f -not -path "*image-build-tools/*" \
+        -type f -not -path "*image-build-base/*" \
+        -type f -name "Dockerfile")
+
     for i in ${__IMAGE_BUILD_REPOS}; do
         echo "$i" | sed 's/\/Dockerfile//g'
     done
+}
+
+# rke2_fecth_chart_index prints out rke2-chasrts index.yaml file
+rke2_charts_get_index() {
+    curl -sL https://raw.githubusercontent.com/rancher/rke2-charts/main/index.yaml
 }
