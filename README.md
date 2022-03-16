f914910e096659a3ce13e9434fb2639bd5858680
Jossemar Cordero <95245208+jossemargt-3pillar@users.noreply.github.com>
2022-03-16 09:47:23 -0600

    Add script to verify if RKE2 charts are up to date (#61)

--- README.md
+++ README.md
@@ -88,14 +88,15 @@ docker run --rm -it rancher/ecm-distro-tools bootstrap_hash -p k3s
 ```sh
 # RKE2
 docker run --rm -it --env GITHUB_TOKEN=<TOKEN> rancher/ecm-distro-tools verify_release_assets v1.23.4
-release: v1.23.4+rke2r1 status: OK
-release: v1.23.4-rc2+rke2r1 status: OK
-release: v1.23.4-rc1+rke2r1 status: MISMATCH
 
 #K3s
 docker run --rm -it --env GITHUB_TOKEN=<TOKEN> rancher/ecm-distro-tools verify_release_assets -r k3s-io/k3s v1.23.4
-release: v1.23.4+k3s1 status: OK
-release: v1.23.4-rc1+k3s1 status: OK
+```
+
+### Verify RKE2 charts are up to date
+
+```sh
+docker run --rm -it --env GITHUB_TOKEN=<TOKEN> rancher/ecm-distro-tools verify_release_assets  verify_rke2_charts -i 'rancher-vsphere-cpi rancher-vsphere-csi' -b 'release-1.22'
 ```
 
 ## Contributing
--- bin/librancher-ecm.sh
+++ bin/librancher-ecm.sh
@@ -1,19 +1,37 @@
 #!/bin/sh
 
-. libstd-ecm.sh
-
-__RANCHER_PATH="${GOPATH}/src/github.com/rancher"
 __INVALID_ARG_ERROR="error: update requires an argument"
-__IMAGE_BUILD_REPOS=$(find "${__RANCHER_PATH}" \
-    -path "*image-build-*"                     \
-    -type f -not -path "*image-build-tools/*"  \
-    -type f -not -path "*image-build-base/*"   \
-    -type f -name "Dockerfile")
 
 # rancher_list_local_repos prints out all of the local
 # repos to STDOUT.
 rancher_list_local_repos() {
+    __RANCHER_PATH="${GOPATH}/src/github.com/rancher"
+    __IMAGE_BUILD_REPOS=$(find "${__RANCHER_PATH}" \
+        -path "*image-build-*" \
+        -type f -not -path "*image-build-tools/*" \
+        -type f -not -path "*image-build-base/*" \
+        -type f -name "Dockerfile")
+
     for i in ${__IMAGE_BUILD_REPOS}; do
         echo "$i" | sed 's/\/Dockerfile//g'
     done
+}
+
+# rke2_list_image_repos prints out all GH repository names of base and rke2 docker images
+rke2_list_image_repos() {
+    curl -s 'https://raw.githubusercontent.com/rancher/rke2/master/developer-docs/image_sources.md' | \
+        grep -E 'rancher/(hardened-build-base|rke2)' | awk -F '|' '{print $4}' | xargs -n1 echo | \
+        sort | uniq
+}
+
+# rke2_list_images prints out all the base and rke2 docker images
+rke2_list_images() {
+    curl -s 'https://raw.githubusercontent.com/rancher/rke2/master/developer-docs/image_sources.md' | \
+        grep -E 'rancher/(hardened-build-base|rke2)' | awk -F '|' '{print $3}' | xargs -n1 echo | \
+        sort | uniq
+}
+
+# rke2_charts_get_index prints out rke2-chasrts index.yaml file
+rke2_charts_get_index() {
+    curl -sL https://raw.githubusercontent.com/rancher/rke2-charts/main/index.yaml
 }
