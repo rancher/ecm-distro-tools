afe8be902bbf023f626848cf18f067facaf1c36d
Brian Downs <brian.downs@gmail.com>
2022-04-15 17:12:32 -0400

    add ability to cut a release and release issue (#90)

--- repository/repository.go
+++ repository/repository.go
@@ -45,6 +45,7 @@ func NewGithub(ctx context.Context, token string) *github.Client {
 	}
 	oauthClient := oauth2.NewClient(ctx, &ts)
 	oauthClient.Timeout = httpTimeout
+
 	return github.NewClient(oauthClient)
 }
 
@@ -70,6 +71,68 @@ func IsValidRepo(repo string) bool {
 	}
 
 	return false
+}
+
+// CreateReleaseOpts
+type CreateReleaseOpts struct {
+	Repo       string
+	Name       string
+	Prerelease bool
+	Branch     string
+}
+
+// CreateRelease
+func CreateRelease(ctx context.Context, client *github.Client, cro *CreateReleaseOpts) (*github.RepositoryRelease, error) {
+	if cro == nil {
+		return nil, errors.New("CreateReleaseOpts cannot be nil")
+	}
+
+	org, err := OrgFromRepo(cro.Repo)
+	if err != nil {
+		return nil, err
+	}
+
+	rr := github.RepositoryRelease{
+		Name:            &cro.Name,
+		TagName:         &cro.Name,
+		Prerelease:      &cro.Prerelease,
+		TargetCommitish: &cro.Branch,
+	}
+	release, _, err := client.Repositories.CreateRelease(ctx, org, cro.Repo, &rr)
+	if err != nil {
+		return nil, err
+	}
+
+	return release, nil
+}
+
+// CreateReleaseIssueOpts
+type CreateReleaseIssueOpts struct {
+	Repo    string
+	Release string
+	Captain string
+}
+
+// CreateReleaseIssue
+func CreateReleaseIssue(ctx context.Context, client *github.Client, cri *CreateReleaseIssueOpts) (*github.Issue, error) {
+	org, err := OrgFromRepo(cri.Repo)
+	if err != nil {
+		return nil, err
+	}
+
+	body := fmt.Sprintf(cutRKE2ReleaseIssue, cri.Release, cri.Release)
+	ir := github.IssueRequest{
+		Title:    types.StringPtr("Cut " + cri.Release),
+		Body:     types.StringPtr(body),
+		Assignee: types.StringPtr(cri.Captain),
+		State:    types.StringPtr("open"),
+	}
+	issue, _, err := client.Issues.Create(ctx, org, cri.Repo, &ir)
+	if err != nil {
+		return nil, err
+	}
+
+	return issue, nil
 }
 
 // RetrieveOriginalIssue
@@ -290,4 +353,49 @@ As always, we welcome and appreciate feedback from our community of users. Pleas
 - [Join our Slack channel](https://slack.rancher.io/)
 - [Check out our documentation](https://rancher.com/docs/k3s/latest/en/) for guidance on how to get started or to dive deep into K3s.
 - [Read how you can contribute here](https://github.com/rancher/k3s/blob/master/CONTRIBUTING.md)
+`
+
+const cutRKE2ReleaseIssue = `**Summary:**
+Task covering patch release work.
+Dev Complete: 1/12 (Typically ~1 week prior to upstream release date)
+**List of required releases:**
+_To release as soon as able for QA:_
+-  %s
+_To release once have approval from QA:_
+-  %[1]s (Never release on a Friday unless specified otherwise)
+**Prep work:**
+- [x] PJM: Dev and QA team to be notified of the incoming releases - add event to team calendar
+- [ ] PJM: Dev and QA team to be notified of the date we will mark the latest release as stable - add event to team calendar [ONLY APPLICABLE FOR LATEST MINOR RELEASE]
+- [ ] PJM: Sync with Rancher PJM to identify applicable Rancer release date
+  - [x] Create tracking issues in rancher/rancher for each Rancher line that the RKE2 release is going into. Assign to release captain. Link to this issue. Ensure it's in the proper milestone by aligning with Rancher PJM.
+ - <UPDATE WITH RANCHER ISSUE>
+  - [ ] Track RKE2 release against the Rancher release date and vice versa. Communicate any changes to Rancher PJM and RKE2 team.
+- [ ] QA: Review changes and understand testing efforts
+- [ ] Release Captain: Prepare release notes in our private [release-notes repo](https://github.com/rancherlabs/release-notes) (submit PR for changes taking care to carefully check links and the components, once merged, create the release in GitHub and mark as a draft and check the pre-release box, fill in title, set target release branch, leave tag version blank for now until we are ready to release)
+- [ ] QA: Validate and close out all issues in the release milestone.
+**Vendor and release work:**
+To find more information on specific steps, please see documentation [here](https://github.com/rancher/rke2/blob/master/developer-docs/upgrading_kubernetes.md)
+- [ ] Release Captain: Tag new Hardened Kubernetes release
+- [ ] Release Captain: Update Helm chart versions
+- [ ] Release Captain: Update RKE2
+- [ ] Release Captain: Tag new RKE2 RC
+- [ ] Release Captain: Tag new RKE2 packaging RC "testing"
+- [ ] Release Captain: Prepare PRs as needed to update [KDM](https://github.com/rancher/kontainer-driver-metadata/) in the appropriate dev branches using an RC.  For more information on the structure of the PR, see the [docs](https://github.com/rancher/rke2/blob/master/developer-docs/upgrading_kubernetes.md#update-rancher-kdm)
+  - [ ] If server args, agent args, or charts are changed, link relevant rancher/rancher issue or create new rancher/rancher issue
+  - [ ] If any new issues are created, escalated to Rancher PJM so they know and can plan for it 
+- [ ] EM: Review and merge above PR
+- [ ] QA: Post merge, run rancher with KDM pointed at the dev branch (where the PR in the previous step was merged) and test import, upgrade, and provisioning against those RCs. This work may be split between Rancher and RKE2 QAs.
+- [ ] Release Captain: Tag the RKE2 release
+- [ ] Release Captain: Add release notes to release
+- [ ] Release Captain: Tag RKE2 packaging release "testing"
+- [ ] Release Captain: Tag RKE2 packaging release "latest"
+**Post-Release work:**
+- [ ] Release Captain: Once release is fully complete (CI is all green and all release artifacts exist), edit the release, uncheck "Pre-release", and save.
+- [ ] Wait 24 hours
+- [ ] Release Captain: Tag RKE2 packaging "stable"
+- [ ] Release Captain: Update stable release in channels.yaml
+- [ ] Release Captain: Prepare PRs as needed to update [KDM](https://github.com/rancher/kontainer-driver-metadata/) in the appropriate dev branches to go from RC to non-RC release. Link this PR to rancher/rancher issue that is tracking the version bump (created in the "Prep work" phase)
+- [ ] EM: Review and merge above PR. Update issue so that QA knows to test
+- [ ] QA: Final validation of above PR and tracked through the linked ticket
+- [ ] PJM: Close the milestone in GitHub.
 `
