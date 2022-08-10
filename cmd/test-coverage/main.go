de8cdc82d5ea87b22b6751ea02543f67d3af5ac3
Derek Nola <derek.nola@suse.com>
2022-08-10 09:58:23 -0700

    Rename tool
    Signed-off-by: Derek Nola <derek.nola@suse.com>

---
+++ cmd/test-coverage/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"os"
+
+	"github.com/sirupsen/logrus"
+	"github.com/urfave/cli"
+)
+
+var (
+	rootFlags = []cli.Flag{
+		&cli.BoolFlag{
+			Name:  "debug,d ",
+			Usage: "debug mode",
+		},
+		&cli.StringFlag{
+			Name:     "program, p",
+			Usage:    "program name [k3s|rke2]",
+			Required: true,
+		},
+		&cli.StringFlag{
+			Name:  "commit, c",
+			Usage: "commit hash to generate coverage report for",
+		},
+	}
+)
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "test-coverage"
+	app.Usage = "Generate coverage report for E2E/Integration tests"
+	app.Flags = rootFlags
+	app.Action = coverage
+	err := app.Run(os.Args)
+	if err != nil {
+		logrus.Fatal(err)
+	}
+
+}
