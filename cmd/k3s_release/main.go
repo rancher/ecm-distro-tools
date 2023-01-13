c597ece8bd022640b59b197e394834cbb392710f
Brian Downs <brian.downs@gmail.com>
2023-01-13 12:38:04 -0700

    make modify-k3s script output in debug, addt updates (#169)

--- cmd/k3s_release/main.go
+++ cmd/k3s_release/main.go
@@ -7,32 +7,30 @@ import (
 	"github.com/urfave/cli"
 )
 
-var (
-	rootFlags = []cli.Flag{
-		cli.StringFlag{
-			Name:   "config",
-			Usage:  "Specify release config file",
-			EnvVar: "RELEASE_CONFIG",
-		},
-		cli.BoolFlag{
-			Name:  "debug",
-			Usage: "Debug mode",
-		},
-	}
-)
+var rootFlags = []cli.Flag{
+	cli.StringFlag{
+		Name:   "config",
+		Usage:  "Specify release config file",
+		EnvVar: "RELEASE_CONFIG",
+	},
+	cli.BoolFlag{
+		Name:  "debug",
+		Usage: "Debug mode",
+	},
+}
 
 func main() {
 	app := cli.NewApp()
 	app.Name = "k3s-release"
-	app.Usage = "Start a k3s release"
+	app.Usage = "Perform a k3s release"
 	app.Commands = []cli.Command{
 		createTagsCommand(),
 		pushTagsCommand(),
 		modifyK3SCommand(),
 	}
 	app.Flags = rootFlags
-	err := app.Run(os.Args)
-	if err != nil {
+
+	if err := app.Run(os.Args); err != nil {
 		logrus.Fatal(err)
 	}
 }
--- cmd/k3s_release/push_tags.go
+++ cmd/k3s_release/push_tags.go
@@ -23,24 +23,30 @@ func pushTagsCommand() cli.Command {
 
 func pushTags(c *cli.Context) error {
 	ctx := context.Background()
+
 	// pushing tags to k3s-io kubernetes fork
 	configPath := c.String("config")
-	release, err := k3s.NewReleaseFromConfig(configPath)
+
+	release, err := k3s.NewRelease(configPath)
 	if err != nil {
 		logrus.Fatalf("failed to read config file: %v", err)
 	}
+
 	client, err := k3s.NewGithubClient(ctx, release.Token)
 	if err != nil {
 		logrus.Fatalf("failed to initialize a new github client from token: %v", err)
 	}
+
 	// this subcommand depends on tag file being created in workspace
 	tags, err := release.TagsFromFile(ctx)
 	if err != nil {
 		logrus.Fatalf("failed to extract tags from file: %v", err)
 	}
+
 	logrus.Infof("pushing tags to github")
 	if err := release.PushTags(ctx, tags, client, k3sRemote); err != nil {
 		logrus.Fatalf("failed to push tags: %v", err)
 	}
+
 	return nil
 }
