5deef18e9496eb9f2e29eac2f92a6531502088bb
Johnatas <you@example.com>
2023-05-23 20:44:28 -0300

    change to ubuntu

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -6,13 +6,23 @@ useradd -m user
 su user
 
 # install go
-VERSION='1.14'
+VERSION='1.17'
 OS='linux'
 ARCH='amd64'
 
 curl -OL https://dl.google.com/go/go${VERSION}.${OS}-${ARCH}.tar.gz
 tar -C /usr/local -xzf go$VERSION.$OS-$ARCH.tar.gz
 rm go$VERSION.$OS-$ARCH.tar.gz
+
+# Install Docker
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
+
+# Add the user to the docker group
+usermod -aG docker user
+
+# Verify Docker installation
+docker version
 
 INSTALLED_GO_VERSION=$(go version)
 echo "Go version ${INSTALLED_GO_VERSION} is installed"
@@ -25,7 +35,6 @@ go get -u github.com/rakyll/hey
 
 # vscode-go dependencies 
 echo "Getting dependencies for the vscode-go plugin "
-# via: https://github.com/microsoft/vscode-go/blob/master/.travis.yml
 go get -u -v github.com/acroca/go-symbols
 go get -u -v github.com/cweill/gotests/...
 go get -u -v github.com/davidrjenni/reftools/cmd/fillstruct
