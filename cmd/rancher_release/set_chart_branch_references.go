bb5123f6c734d47e5c63545af720265fefbb0c2f
Pedro Tashima <pedro.tashima@suse.com>
2023-10-10 17:12:24 -0300

    add short flags

--- cmd/rancher_release/set_chart_branch_references.go
+++ cmd/rancher_release/set_chart_branch_references.go
@@ -3,48 +3,61 @@ package main
 import (
 	"context"
 	"errors"
-	"os"
 
 	"github.com/rancher/ecm-distro-tools/release/rancher"
-	"github.com/urfave/cli"
+	"github.com/urfave/cli/v2"
 )
 
-func setChartsBranchReferencesCommand() cli.Command {
-	return cli.Command{
+func setChartsBranchReferencesCommand() *cli.Command {
+	return &cli.Command{
 		Name:  "set-charts-branch-refs",
 		Usage: "set charts branch references in files",
 		Flags: []cli.Flag{
-			cli.StringFlag{
+			&cli.StringFlag{
 				Name:     "fork-path",
+				Aliases:  []string{"f"},
 				Usage:    "rancher repo fork directory path",
 				Required: true,
 			},
-			cli.StringFlag{
+			&cli.StringFlag{
 				Name:     "base-branch",
+				Aliases:  []string{"b"},
 				Usage:    "rancher branch to use as a base, e.g: release/v2.8",
 				Required: true,
 			},
-			cli.StringFlag{
+			&cli.StringFlag{
 				Name:     "current-charts-branch",
+				Aliases:  []string{"c"},
 				Usage:    "current branch set for charts in the repo",
 				Required: true,
 			},
-			cli.StringFlag{
+			&cli.StringFlag{
 				Name:     "new-charts-branch",
+				Aliases:  []string{"n"},
 				Usage:    "branch to be replaced in charts in the repo",
 				Required: true,
 			},
-			cli.BoolFlag{
-				Name:  "create-pr",
-				Usage: "if true, a PR will be created from your fork to the rancher repo base branch and a variable 'GITHUB_TOKEN' must be exported",
+			&cli.BoolFlag{
+				Name:    "create-pr",
+				Aliases: []string{"p"},
+				Usage:   "if true, a PR will be created from your fork to the rancher repo base branch and a variable 'GITHUB_TOKEN' must be exported",
 			},
-			cli.StringFlag{
+			&cli.StringFlag{
 				Name:     "fork-owner",
+				Aliases:  []string{"o"},
 				Usage:    "github username of the owner of the fork, only required if 'create-pr' is true",
 				Required: false,
 			},
-			cli.BoolFlag{
+			&cli.StringFlag{
+				Name:     "github-token",
+				Aliases:  []string{"g"},
+				Usage:    "github token",
+				EnvVars:  []string{"GITHUB_TOKEN"},
+				Required: false,
+			},
+			&cli.BoolFlag{
 				Name:     "dry-run",
+				Aliases:  []string{"r"},
 				Usage:    "the newly created branch won't be pushed to remote and the PR won't be created",
 				Required: false,
 			},
@@ -58,10 +71,10 @@ func setChartBranchReferences(c *cli.Context) error {
 	baseBranch := c.String("base-branch")
 	currentBranch := c.String("current-charts-branch")
 	newBranch := c.String("new-charts-branch")
-	createPR := c.BoolT("create-pr")
+	createPR := c.Bool("create-pr")
 	forkOwner := c.String("fork-owner")
-	githubToken := os.Getenv("GITHUB_TOKEN")
-	dryRun := c.BoolT("dry-run")
+	githubToken := c.String("github-token")
+	dryRun := c.Bool("dry-run")
 	if createPR {
 		if forkOwner == "" {
 			return errors.New("'create-pr' requires 'fork-owner'")
