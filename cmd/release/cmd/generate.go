332af7020134714e2c0bef3f04006c272dd751b4
Brian Downs <brian.downs@gmail.com>
2024-01-08 19:57:54 -0700

    release notes functionality
    Signed-off-by: Brian Downs <brian.downs@gmail.com>

--- cmd/release/cmd/generate.go
+++ cmd/release/cmd/generate.go
@@ -1,9 +1,18 @@
 package cmd
 
 import (
+	"context"
+	"fmt"
 	"os"
 
+	"github.com/rancher/ecm-distro-tools/release"
+	"github.com/rancher/ecm-distro-tools/repository"
 	"github.com/spf13/cobra"
+)
+
+var (
+	prevMilestone *string
+	milestone     *string
 )
 
 // generateCmd represents the generate command
@@ -12,18 +21,42 @@ var generateCmd = &cobra.Command{
 	Short: "",
 	Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
-		if len(args) < 1 {
+		if len(args) < 1 || len(args) > 2 {
 			rootCmd.Help()
 			os.Exit(0)
 		}
 
+		ctx := context.Background()
+		client := repository.NewGithub(ctx, rootConfig.Auth.GithubToken)
+
+		var (
+			owner string
+			repo  string
+		)
+
 		switch args[0] {
 		case "k3s":
+			if args[1] == "release-notes" {
+				owner = "k3s-io"
+				repo = "k3s"
+			}
 		case "rke2":
+			if args[1] == "release-notes" {
+				owner = "rancher"
+				repo = "rke2"
+			}
 		default:
 			rootCmd.Help()
 			os.Exit(0)
 		}
+
+		notes, err := release.GenReleaseNotes(ctx, owner, repo, *milestone, *prevMilestone, client)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		fmt.Print(notes.String())
 	},
 }
 
@@ -38,5 +71,6 @@ func init() {
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	// generateCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	prevMilestone = generateCmd.Flags().StringP("prev-milestone", "p", "", "Previous Milestone")
+	milestone = generateCmd.Flags().StringP("milestone", "m", "", "Milestone")
 }
