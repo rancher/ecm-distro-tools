1958374d10a957fecc220c50ea1d06b90105e419
Brian Downs <brian.downs@gmail.com>
2024-01-16 14:20:13 -0700

    comment
    Signed-off-by: Brian Downs <brian.downs@gmail.com>

--- cmd/release/cmd/root.go
+++ cmd/release/cmd/root.go
@@ -23,6 +23,7 @@ var rootCmd = &cobra.Command{
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	if err := rootCmd.Execute(); err != nil {
+		fmt.Println("error: " + err.Error())
 		os.Exit(1)
 	}
 }
