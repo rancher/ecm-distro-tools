db7da76c226568e4a3d787aeacd58c332b65084d
nicholasSSUSE <nicholas.paolillo@suse.com>
2024-08-02 17:11:38 -0300

    new Update function for executing chart release/forward-port

--- release/charts/charts.go
+++ release/charts/charts.go
@@ -7,6 +7,8 @@ import (
 	"os/exec"
 	"strings"
 
+	"github.com/go-git/go-git/v5"
+	"github.com/google/go-github/v39/github"
 	"github.com/rancher/ecm-distro-tools/cmd/release/config"
 )
 
@@ -28,6 +30,42 @@ func List(ctx context.Context, c *config.ChartsRelease, branch, chart string) (s
 	return response, nil
 }
 
+// Update will pull the target chart version to the local branch and create a PR to release the chart
+func Update(ctx context.Context, gh *github.Client, c *config.ChartsRelease, br, ch, vr string) (string, error) {
+	var branchArg, chartArg, versionArg, forkArg string
+
+	branchArg = "--branch-version=" + br
+	chartArg = "--chart=" + ch
+	versionArg = "--version=" + vr
+	forkArg = "--fork=" + c.ChartsForkURL
+
+	output, err := runChartsBuild(c.Workspace, "release", branchArg, chartArg, versionArg, forkArg)
+	if err != nil {
+		return string(output), err
+	}
+
+	r, err := git.PlainOpen(c.Workspace)
+	if err != nil {
+		return string(output), err
+	}
+
+	wt, err := r.Worktree()
+	if err != nil {
+		return string(output), err
+	}
+
+	if err := wt.AddWithOptions(&git.AddOptions{All: true}); err != nil {
+		return string(output), err
+	}
+
+	commitMsg := "release chart: " + ch + " - version: " + vr
+	if _, err := wt.Commit(commitMsg, &git.CommitOptions{All: true}); err != nil {
+		return string(output), err
+	}
+
+	return string(output), nil
+}
+
 func runChartsBuild(chartsRepoPath string, args ...string) ([]byte, error) {
 	// save current working dir
 	ecmWorkDir, err := os.Getwd()
@@ -45,12 +83,12 @@ func runChartsBuild(chartsRepoPath string, args ...string) ([]byte, error) {
 	cmd := exec.Command(bin, args...)
 	output, err := cmd.CombinedOutput()
 	if err != nil {
-		return nil, err
+		return output, err
 	}
 
 	// Change back working dir for the caller
 	if err := os.Chdir(ecmWorkDir); err != nil {
-		return nil, err
+		return output, err
 	}
 
 	return output, nil
