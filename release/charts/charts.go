ba88a1845e642dc2e514fcf83c46f369e13cd733
nicholasSSUSE <nicholas.paolillo@suse.com>
2024-08-09 17:44:33 -0300

    updating release/charts with push execution and pr creation

--- release/charts/charts.go
+++ release/charts/charts.go
@@ -4,13 +4,50 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"log"
 	"os"
 	"os/exec"
 	"strings"
 
+	"github.com/google/go-github/v39/github"
+
 	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing"
+	"github.com/go-git/go-git/v5/plumbing/object"
+	"github.com/go-git/go-git/v5/plumbing/storer"
+
 	"github.com/rancher/ecm-distro-tools/cmd/release/config"
+	ecmExec "github.com/rancher/ecm-distro-tools/exec"
+	"github.com/rancher/ecm-distro-tools/repository"
 )
+
+// body is the default PR body for the charts release PR
+const body string = `
+## Charts Checklist (built for v0.9.5 charts-build-scripts)
+
+### Checkpoint 0: Validate **release.yaml**
+
+Validation steps:
+- [ ] Each chart version in **release.yaml** DOES NOT modify an already released chart. If so, stop and modify the versions so that it releases a net-new chart.
+- [ ] Each chart version in **release.yaml** IS exactly 1 more patch version than the last released chart version. If not, stop and modify the versions so that it releases a net-new chart.
+
+### Checkpoint 1: Compare contents of assets/ to charts/
+
+Validation steps:
+- [ ] Running **make unzip** to regenerate the **charts/** from scratch, then **git diff** to check differences between **assets/** and **charts/** yields NO differences or innocuous differences.
+
+IMPORTANT: Do not undo these changes for future steps since we want to keep the charts/ that match the current contents of assets!
+
+### Checkpoint 2: Compare assets against index.yaml
+
+Validation steps:
+- [ ] The **index.yaml** file has an entry for each chart version.
+- [ ] The **index.yaml** entries for each chart matches the **Chart.yaml** for each chart.
+- [ ] Each chart has ALL required annotations
+- kube-version annotation
+- rancher-version annotation
+- permits-os annotation (indicates Windows and/or Linux)
+`
 
 // List prints the lifecycle status of the charts
 func List(ctx context.Context, c *config.ChartsRelease, branch, chart string) (string, error) {
@@ -92,4 +129,104 @@ func runChartsBuild(chartsRepoPath string, args ...string) ([]byte, error) {
 	}
 
 	return output, nil
+}
+
+// Push will push the charts updates to the remote upstream charts repository
+func Push(ctx context.Context, c *config.ChartsRelease, u *config.User, ghc *github.Client, b, t string, d bool) (string, error) {
+	const repoOwner = "rancher"
+	const repoName = "charts"
+
+	var (
+		r      *git.Repository     // local opened repository
+		h      *plumbing.Reference // local head reference
+		remote string              // remote repository name
+		err    error
+	)
+
+	r, err = git.PlainOpen(c.Workspace)
+	if err != nil {
+		return "", err
+	}
+
+	remote, err = repository.GetUpstreamRemote(r, c.ChartsRepoURL)
+	if err != nil {
+		return "", err
+	}
+
+	h, err = r.Head()
+	if err != nil {
+		return "", err
+	}
+
+	// create a new PR
+	pr := &github.NewPullRequest{
+		Title:               github.String(fmt.Sprintf("[%s] batch release", b)),
+		Base:                github.String(b),
+		Head:                github.String(h.Name().Short()),
+		Body:                github.String(body),
+		MaintainerCanModify: github.Bool(true),
+	}
+
+	// debug mode
+	if d {
+		if err := debugPullRequest(r, remote, b); err != nil {
+			return "", err
+		}
+	}
+
+	if err := repository.PushRemoteBranch(r, remote, u.GithubUsername, t, d); err != nil {
+		return "", err
+	}
+
+	prResp, _, err := ghc.PullRequests.Create(ctx, repoOwner, repoName, pr)
+	if err != nil {
+		return "", err
+	}
+
+	return prResp.GetHTMLURL(), nil
+}
+
+// debugPullRequest will prompt the user to check the files and commits that will be pushed to the remote repository
+func debugPullRequest(r *git.Repository, remote, b string) error {
+	var (
+		execute bool
+		iter    object.CommitIter
+		err     error
+	)
+
+	if execute = ecmExec.UserInput("Check files that will be pushed?"); execute {
+		// commit history
+		iter, err = r.Log(&git.LogOptions{})
+		if err != nil {
+			return err
+		}
+
+		err = iter.ForEach(func(c *object.Commit) error {
+			fileStats, err := c.Stats()
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			fmt.Println(c)
+			fmt.Println(fileStats)
+
+			if execute = ecmExec.UserInput("Check next files that will be pushed?"); !execute {
+				return storer.ErrStop
+			}
+
+			return nil
+		})
+	}
+
+	if execute = ecmExec.UserInput("Check commits that will be pushed?"); execute {
+		if err := repository.DiffLocalToRemote(r, remote, b); err != nil {
+			return err
+		}
+	}
+
+	if execute = ecmExec.UserInput("Push and Create PR to the remote repository?"); !execute {
+		return errors.New("user aborted the push")
+	}
+
+	return nil
 }
