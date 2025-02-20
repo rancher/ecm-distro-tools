516f3605b05cb7e22f41cd3fe879023ccdd4c604
Brooks Newberry <brooks@newberry.com>
2025-02-19 17:14:36 -0800

    concurrency
    Signed-off-by: Brooks Newberry <brooks@newberry.com>

--- release/rke2/images.go
+++ release/rke2/images.go
@@ -6,9 +6,11 @@ import (
 	"io"
 	"io/fs"
 	"strings"
+	"sync"
 
 	"github.com/google/go-containerregistry/pkg/name"
 	reg "github.com/rancher/ecm-distro-tools/registry"
+	"golang.org/x/sync/errgroup"
 )
 
 // RegistryClient defines the interface for interacting with container registries
@@ -44,18 +46,18 @@ type Image struct {
 }
 
 type ReleaseInspector struct {
-	fs    fs.FS
-	oss   RegistryClient
-	prime RegistryClient
-	debug bool
+	assets fs.FS
+	oss    RegistryClient
+	prime  RegistryClient
+	debug  bool
 }
 
 func NewReleaseInspector(fs fs.FS, oss, prime RegistryClient, debug bool) *ReleaseInspector {
 	return &ReleaseInspector{
-		fs:    fs,
-		oss:   oss,
-		prime: prime,
-		debug: debug,
+		assets: fs,
+		oss:    oss,
+		prime:  prime,
+		debug:  debug,
 	}
 }
 
@@ -75,22 +77,33 @@ func (r *ReleaseInspector) InspectRelease(ctx context.Context, version string) (
 // imageMap reads per-platform image list files and coalesces them
 // into one map to collect images for all platforms.
 func (r *ReleaseInspector) imageMap() (map[string]ReleaseImage, error) {
-	amd64Images, err := r.readImageList(ListLinuxAmd64)
-	if err != nil {
+	// download image lists for release
+	var (
+		amd64Images []string
+		arm64Images []string
+		winImages   []string
+	)
+
+	g := new(errgroup.Group)
+
+	g.Go(func() (err error) {
+		amd64Images, err = r.readImageList(ListLinuxAmd64)
+		return err
+	})
+	g.Go(func() (err error) {
+		arm64Images, err = r.readImageList(ListLinuxArm64)
+		return err
+	})
+	g.Go(func() (err error) {
+		winImages, err = r.readImageList(ListWindowsAmd64)
+		return
+	})
+
+	if err := g.Wait(); err != nil {
 		return nil, err
 	}
 
-	arm64Images, err := r.readImageList(ListLinuxArm64)
-	if err != nil {
-		return nil, err
-	}
-
-	winImages, err := r.readImageList(ListWindowsAmd64)
-	if err != nil {
-		return nil, err
-	}
-
-	// merge all images into a single map
+	// merge all images into a map
 	imageMap := make(map[string]ReleaseImage)
 	for _, imagePair := range [][2]interface{}{
 		{amd64Images, LinuxAmd64},
@@ -130,7 +143,7 @@ func (r *ReleaseInspector) imageMap() (map[string]ReleaseImage, error) {
 
 // readImageList reads an image list file and returns its contents
 func (r *ReleaseInspector) readImageList(filename string) ([]string, error) {
-	file, err := r.fs.Open(filename)
+	file, err := r.assets.Open(filename)
 	if err != nil {
 		return nil, err
 	}
@@ -146,29 +159,43 @@ func (r *ReleaseInspector) readImageList(filename string) ([]string, error) {
 
 // checkImages checks if the required images exist in the OSS and Prime registries
 func (r *ReleaseInspector) checkImages(ctx context.Context, requiredImages map[string]ReleaseImage) ([]Image, error) {
-	var results []Image
+	resultChan := make(chan Image, len(requiredImages))
+	var wg sync.WaitGroup
 
 	for _, required := range requiredImages {
-		ossImage, err := r.oss.Image(ctx, required.Reference)
-		if err != nil {
-			ossImage = reg.Image{
-				Exists:    false,
-				Platforms: make(map[reg.Platform]bool),
+		wg.Add(1)
+		go func(img ReleaseImage) {
+			defer wg.Done()
+
+			ossImage, err := r.oss.Image(ctx, img.Reference)
+			if err != nil {
+				ossImage = reg.Image{
+					Exists:    false,
+					Platforms: make(map[reg.Platform]bool),
+				}
 			}
-		}
 
-		var primeImage reg.Image
-		if r.prime != nil {
-			primeImage, _ = r.prime.Image(ctx, required.Reference)
-		}
+			var primeImage reg.Image
+			if r.prime != nil {
+				primeImage, _ = r.prime.Image(ctx, img.Reference)
+			}
 
-		status := Image{
-			ReleaseImage: required,
-			OSSImage:     ossImage,
-			PrimeImage:   primeImage,
-		}
+			resultChan <- Image{
+				ReleaseImage: img,
+				OSSImage:     ossImage,
+				PrimeImage:   primeImage,
+			}
+		}(required)
+	}
 
-		results = append(results, status)
+	go func() {
+		wg.Wait()
+		close(resultChan)
+	}()
+
+	var results []Image
+	for img := range resultChan {
+		results = append(results, img)
 	}
 
 	return results, nil
