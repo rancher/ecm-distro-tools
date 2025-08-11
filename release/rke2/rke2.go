4dde64451f20e0724345c589ad8111801fdf7c46
Rafael Breno <rafael_breno@outlook.com>
2025-08-11 17:05:13 -0300

    Add dynamic Alpine Version

--- release/rke2/rke2.go
+++ release/rke2/rke2.go
@@ -4,7 +4,9 @@ import (
 	"context"
 	"encoding/json"
 	"errors"
+	"fmt"
 	"net/http"
+	"regexp"
 	"strings"
 	"time"
 
@@ -16,6 +18,7 @@ import (
 
 const (
 	goDevURL           = "https://go.dev/dl/?mode=json"
+	dockerHubTagsURL   = "https://hub.docker.com/v2/repositories/library/golang/tags"
 	imageBuildBaseRepo = "image-build-base"
 )
 
@@ -24,7 +27,7 @@ type goVersionRecord struct {
 	Stable  bool   `json:"stable"`
 }
 
-func ImageBuildBaseRelease(ctx context.Context, ghClient *github.Client, alpineVersion string, dryRun bool) error {
+func ImageBuildBaseRelease(ctx context.Context, ghClient *github.Client, dryRun bool) error {
 	versions, err := goVersions(goDevURL)
 	if err != nil {
 		return err
@@ -36,14 +39,22 @@ func ImageBuildBaseRelease(ctx context.Context, ghClient *github.Client, alpineV
 			logrus.Info("version " + version.Version + " is not stable")
 			continue
 		}
-		version := strings.Split(version.Version, "go")[1]
-		alpineTag := version + "-alpine" + alpineVersion
+		goVersion := strings.Split(version.Version, "go")[1]
+
+		// Dynamically find the Alpine version for this Go version.
+		alpineVersion, err := getAlpineVersionForGo(goVersion)
+		if err != nil {
+			return fmt.Errorf("failed to find a corresponding alpine version for go %s: %v", goVersion, err)
+		}
+		logrus.Infof("found alpine v%s for go v%s", alpineVersion, goVersion)
+
+		alpineTag := goVersion + "-alpine" + alpineVersion
 
 		if err := docker.CheckImageArchs(ctx, "library", "golang", alpineTag, []string{"amd64", "arm64", "s390x"}); err != nil {
-			return err
+			return fmt.Errorf("failed to check image archs for %s: %v", alpineTag, err)
 		}
 
-		imageBuildBaseTag := "v" + version + "b1"
+		imageBuildBaseTag := "v" + goVersion + "b1"
 		logrus.Info("stripped version: " + imageBuildBaseTag)
 		if _, _, err := ghClient.Repositories.GetReleaseByTag(ctx, "rancher", imageBuildBaseRepo, imageBuildBaseTag); err == nil {
 			logrus.Info("release " + imageBuildBaseTag + " already exists")
@@ -66,6 +77,51 @@ func ImageBuildBaseRelease(ctx context.Context, ghClient *github.Client, alpineV
 		logrus.Info("created release for version: " + imageBuildBaseTag)
 	}
 	return nil
+}
+
+// dockerHubResponse defines the structure for the Docker Hub API response.
+type dockerHubResponse struct {
+	Next    string `json:"next"`
+	Results []struct {
+		Name string `json:"name"`
+	} `json:"results"`
+}
+
+// getAlpineVersionForGo queries the Docker Hub API to find the Alpine version
+// associated with a specific Go version.
+func getAlpineVersionForGo(goVersion string) (string, error) {
+	// Compile regex to find a tag like "1.22.5-alpine3.20" and extract "3.20"
+	re := regexp.MustCompile(fmt.Sprintf(`^%s-alpine(\d+\.\d+)$`, regexp.QuoteMeta(goVersion)))
+
+	client := ecmHTTP.NewClient(time.Second * 15)
+	url := dockerHubTagsURL
+
+	for url != "" {
+		res, err := client.Get(url)
+		if err != nil {
+			return "", err
+		}
+		defer res.Body.Close()
+
+		if res.StatusCode != http.StatusOK {
+			return "", errors.New("failed to query docker hub, status: " + res.Status)
+		}
+
+		var resp dockerHubResponse
+		if err := json.NewDecoder(res.Body).Decode(&resp); err != nil {
+			return "", err
+		}
+
+		for _, result := range resp.Results {
+			matches := re.FindStringSubmatch(result.Name)
+			if len(matches) > 1 {
+				return matches[1], nil // Return the first captured group (the version number)
+			}
+		}
+		url = resp.Next // Move to the next page
+	}
+
+	return "", errors.New("no matching alpine tag found for go version " + goVersion)
 }
 
 func goVersions(goDevURL string) ([]goVersionRecord, error) {
