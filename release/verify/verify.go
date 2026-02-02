8c812575be9131ca81342d374d9f0cf631f6cdb4
Rafael Breno <rafael_breno@outlook.com>
2026-02-02 15:46:01 -0300

    Add verify package that stores verify related cmd

---
+++ release/verify/verify.go
@@ -0,0 +1,174 @@
+// Package verify stores the logic to validate the current release, if it's respecting the release process.
+package verify
+
+import (
+	"context"
+	"fmt"
+	"regexp"
+	"strings"
+
+	"github.com/google/go-github/v81/github"
+	"github.com/rancher/ecm-distro-tools/cmd/release/config"
+	"github.com/rancher/ecm-distro-tools/repository"
+)
+
+var (
+	// rpmRegex matches the RKE2 RPM version format:
+	// v<Major>.<Minor>.<Patch>+rke2r<Revision>.<Channel>.<Build>
+	// Example: v1.23.6+rke2r1.stable.0
+	// No need to support RCs as this will never receive an RC/Alpha versions
+	rpmRegex = regexp.MustCompile(`^v\d{1,2}\.\d{1,2}\.\d{1,2}\+rke2r\d\.(testing|latest|stable)\.\d$`)
+
+	// channelRegex matches the RKE2 RPM channel format:
+	channelRegex = regexp.MustCompile(`.(testing|latest|stable)\.\d$`)
+)
+
+func GA(ctx context.Context, ghClient *github.Client, version string) error {
+	switch {
+	// The rpmRegex should be the first case to avoid RKE2 releases being pulled in for containing rke2r*
+	case rpmRegex.MatchString(version):
+		if err := verifyRPMGA(ctx, ghClient, version); err != nil {
+			return err
+		}
+	case strings.Contains(version, "rke2"):
+		if err := verifyRKE2(ctx, ghClient, version); err != nil {
+			return err
+		}
+
+	case strings.Contains(version, "k3s"):
+		if err := verifyK3s(ctx, ghClient, version); err != nil {
+			return err
+		}
+	// Rancher versions will be handled in default
+	default:
+		if err := verifyRancher(ctx, ghClient, version); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func verifyRKE2(ctx context.Context, ghClient *github.Client, version string) error {
+	versionWithRC := appendRC1(version)
+	rel, err := repository.RetrieveRelease(
+		ctx,
+		ghClient,
+		config.RancherGithubOrganization,
+		config.RKE2RepositoryName,
+		versionWithRC,
+	)
+	if err != nil {
+		return err
+	}
+	if rel == nil {
+		return fmt.Errorf("RKE2 RC release '%s' not found", versionWithRC)
+	}
+	return nil
+}
+
+func verifyK3s(ctx context.Context, ghClient *github.Client, version string) error {
+	versionWithRC := appendRC1(version)
+	rel, err := repository.RetrieveRelease(
+		ctx,
+		ghClient,
+		config.K3sGithubOrganization,
+		config.K3sRepositoryName,
+		versionWithRC,
+	)
+	if err != nil {
+		return err
+	}
+	if rel == nil {
+		return fmt.Errorf("K3s RC release '%s' not found", versionWithRC)
+	}
+	return nil
+}
+
+func verifyRancher(ctx context.Context, ghClient *github.Client, version string) error {
+	versionWithRC := appendRC1(version)
+	rel, err := repository.RetrieveRelease(
+		ctx,
+		ghClient,
+		config.RancherGithubOrganization,
+		config.RancherRepositoryName,
+		versionWithRC,
+	)
+	if err != nil {
+		return err
+	}
+	if rel == nil {
+		return fmt.Errorf("rancher RC release '%s' not found", versionWithRC)
+	}
+	return nil
+}
+
+func verifyRPMGA(ctx context.Context, ghClient *github.Client, version string) error {
+	// For RPM 'testing' GA releases it SHOULD have a 'testing' RC release
+	if strings.Contains(version, "testing") {
+		versionWithRC := appendRC1(version)
+		rel, err := repository.RetrieveRelease(
+			ctx,
+			ghClient,
+			config.RancherGithubOrganization,
+			config.RPMRepositoryName,
+			versionWithRC,
+		)
+		if err != nil {
+			return err
+		}
+		if rel == nil {
+			return fmt.Errorf("RPM RC release '%s' not found", versionWithRC)
+		}
+		return nil
+	}
+
+	// For RPM 'latest' releases it SHOULD have a 'testing' release
+	if strings.Contains(version, "latest") {
+		testingVersion := channelRegex.ReplaceAllString(version, "testing.0")
+		rel, err := repository.RetrieveRelease(
+			ctx,
+			ghClient,
+			config.RancherGithubOrganization,
+			config.RPMRepositoryName,
+			testingVersion,
+		)
+		if err != nil {
+			return err
+		}
+		if rel == nil {
+			return fmt.Errorf("RPM testing channel release '%s' not found", testingVersion)
+		}
+		return nil
+	}
+
+	// For RPM 'stable' releases it SHOULD have a 'latest' release
+	if strings.Contains(version, "stable") {
+		testingVersion := channelRegex.ReplaceAllString(version, "latest.0")
+		rel, err := repository.RetrieveRelease(
+			ctx,
+			ghClient,
+			config.RancherGithubOrganization,
+			config.RPMRepositoryName,
+			testingVersion,
+		)
+		if err != nil {
+			return err
+		}
+		if rel == nil {
+			return fmt.Errorf("RPM latest channel release '%s' not found", testingVersion)
+		}
+		return nil
+	}
+	return nil
+}
+
+func appendRC1(version string) string {
+	switch {
+	// In this switch we can omit the case for RPM because
+	// it would be the same process for RPMs, K3s and RKE2 releases.
+	case strings.Contains(version, "rke2"), strings.Contains(version, "k3s"):
+		return strings.ReplaceAll(version, "+", "-rc1+")
+	default:
+		return version + "-rc1"
+	}
+}
