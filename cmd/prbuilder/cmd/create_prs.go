8c583594576e66dbafa4b80f5beb07984355fd4d
Dan Pock <self@danpock.me>
2026-07-10 15:59:23 -0400

    move `prbuilder` under `cmd` for better project structure consistency

--- cmd/prbuilder/cmd/create_prs.go
+++ cmd/prbuilder/cmd/create_prs.go
@@ -6,7 +6,7 @@ import (
 	"os"
 
 	"github.com/rancher/ecm-distro-tools/cmd/prbuilder/config"
-	"github.com/rancher/ecm-distro-tools/prbuilder"
+	"github.com/rancher/ecm-distro-tools/cmd/prbuilder/prbuilder"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
